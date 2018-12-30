6ac037d55ac5330cde62d8c5c6f70226b3f0cec5
dpoit <dannypoit@gmail.com>
2018-12-29 21:07:59 -0500

    Fixed tests for update action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,15 +50,26 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = Post.find_by_id(params[:id])
+    return render_not_found if @post.blank?
+
+    @post.update_attributes(post_params)
+
+    if @post.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
     end
+    
+    # respond_to do |format|
+    #   if @post.update(post_params)
+    #     format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @post }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @post.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /posts/1
