7c471555e78989a80bfba659a2861be53d54ab07
dpoit <dannypoit@gmail.com>
2018-12-29 21:22:40 -0500

    Fixed delete action tests and cleaned up some unused scaffolding code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  resources :posts, only: [:new, :create, :show, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
 end
