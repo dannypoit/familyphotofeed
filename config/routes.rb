97dfd3c50d3bce7f7f131000da5d4963bc161c00
dpoit <dannypoit@gmail.com>
2019-01-18 16:19:28 -0500

    Set index to default to login page when not signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root "posts#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  root 'posts#index'
+
   resources :posts do
     resources :comments, only: :create
   end
