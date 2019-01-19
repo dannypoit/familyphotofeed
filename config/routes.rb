512bb4e7ac4793cd5476da40390547c2e9c9d436
dpoit <dannypoit@gmail.com>
2019-01-19 10:17:32 -0500

    Removed routing for auth/unauth users and added log in link back to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-  devise_scope :user do
-    authenticated :user do
-      root 'posts#index', as: :authenticated_root
-    end
-
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root 'posts#index', as: :authenticated_root
+  #   end
+  #
+  #   unauthenticated do
+  #     root 'devise/sessions#new', as: :unauthenticated_root
+  #   end
+  # end
 
   root 'posts#index'
 
