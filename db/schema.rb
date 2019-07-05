73452e7b09a6285fdcf63965802e4e7ccf469ebf
dpoit <dannypoit@gmail.com>
2019-07-04 21:37:57 -0400

    Added family column to user table and displayed family avatar on profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227235353) do
+ActiveRecord::Schema.define(version: 20190704204357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20190227235353) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "avatar"
+    t.integer  "family",                 default: [],              array: true
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
