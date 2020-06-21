219fb3d9b71f8486d75c2065505aeac641c31be1
dannypoit <danny@dannypoit.com>
2020-06-21 01:04:16 -0400

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-
-  # devise_scope :user do
-  #   authenticated :user do
-  #     root 'posts#index', as: :authenticated_root
-  #   end
-  #
-  #   unauthenticated do
-  #     root 'devise/sessions#new', as: :unauthenticated_root
-  #   end
-  # end
-
   root 'posts#index'
-
-  resources :posts do
-    resources :comments, only: :create
-  end
-
-  resources :users, only: [:show, :edit, :update]
-
   get '/users/:id/change_avatar', to: 'users#change_avatar', as: 'change_avatar'
-
   post "/friends/add" => "friends/add"
   post "/friends/reject" => "friends/reject"
   post "/friends/remove" => "friends/remove"
   get "/friends/search" => "friends/search"
   post "/friends/search" => "friends/search"
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :friends, only: [:index, :create]
+  resources :users, only: [:show, :edit, :update]
 end
