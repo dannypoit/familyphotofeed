1019d0de8a4f431628717963ee85224e95a68ff3
dannypoit <danny@dannypoit.com>
2020-06-24 16:20:07 -0400

    Reduce comment min length to 1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :message, presence: true, length: { minimum: 2, maximum: 240 }
+  validates :message, presence: true, length: { minimum: 1, maximum: 240 }
 
   belongs_to :post
   belongs_to :user
