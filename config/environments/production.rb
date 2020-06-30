9fca9f51b47e7e8027b57ef89bade76dee5b46d0
dannypoit <danny@dannypoit.com>
2020-06-30 09:33:11 -0400

    Update action mailer on prod to send using SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   config.action_mailer.default_url_options = { host: 'familyphotofeed-dpoit.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
