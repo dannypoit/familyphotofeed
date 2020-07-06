0264e5911b3cac870cc67f926e92d730b45c17ab
dannypoit <danny@dannypoit.com>
2020-07-06 10:37:41 -0400

    Fix first test for comments controller spec

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,3 +97,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rails.application.routes.default_url_options[:host] = "http://www.familyphotofeed.com"
