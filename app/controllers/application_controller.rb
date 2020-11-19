3ac3d979ce4b80953bf2d2498c1d2b38971cda6f
dannypoit <danny@dannypoit.com>
2020-11-18 23:10:52 -0500

    Update app controller to fix login issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   def render_not_found(status=:not_found)
     render plain: "#{status.to_s.titleize}", status: status
