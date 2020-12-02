b12124999e6e6827c24bebb086eafa292196fc70
dannypoit <danny@dannypoit.com>
2020-12-02 13:02:28 -0500

    Add protect from forgery to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery
 
   def render_not_found(status=:not_found)
     render plain: "#{status.to_s.titleize}", status: status
