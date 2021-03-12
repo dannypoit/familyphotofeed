287227a6de51215fe35d00e327d95dc30ff3f5a1
dannypoit <danny@dannypoit.com>
2021-03-11 22:43:40 -0500

    Tested API key with Basic auth in dev and matched settings on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => 'apikey',
     :password => ENV['SENDGRID_API_KEY'],
